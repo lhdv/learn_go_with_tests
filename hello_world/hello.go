432dba785b1310b35ca18dea75c3aa5125c4e8ab
LH <lh@lhdv.net>
2019-10-28 13:40:47 -0300

    First test on Hello() function

---
+++ hello_world/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// Hello returns the classic Hello World string
+func Hello() string {
+	return "Hello World of Testing!!!"
+}
+
+func main() {
+	fmt.Println(Hello())
+}
