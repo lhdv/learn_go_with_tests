5f3e70511a5e124799b13100113d448932fd31b7
LH <lh@lhdv.net>
2019-10-29 18:27:37 -0300

    Iteration test
    - Write a new iteration on Repeat()

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,11 +1,13 @@
 package iteration
 
+const repeatCount = 5
+
 // Repeat function returns a string repeated 5 times
 func Repeat(char string) string {
 	var repeated string
 
-	for i := 0; i < 5; i++ {
-		repeated = repeated + char
+	for i := 0; i < repeatCount; i++ {
+		repeated += char
 	}
 
 	return repeated
