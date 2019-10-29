abb54f39dbccdfa7db2c9317c36708bd90ee4989
LH <lh@lhdv.net>
2019-10-29 18:40:02 -0300

    Iteration test
    - Refactor Repeat() function to receive how many times you want to
    repeat the string
    - Add an example in test file

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,11 +2,17 @@ package iteration
 
 const repeatCount = 5
 
-// Repeat function returns a string repeated 5 times
-func Repeat(char string) string {
+// Repeat function returns a string repeated how many times you ask.
+// If you don't know how many times you want, the default is 5 by the way.
+func Repeat(char string, times int) string {
+
 	var repeated string
 
-	for i := 0; i < repeatCount; i++ {
+	if times <= 0 {
+		times = repeatCount
+	}
+
+	for i := 0; i < times; i++ {
 		repeated += char
 	}
 
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,18 +1,27 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
 
 	if repeated != expected {
 		t.Errorf("expected %q, got %q", expected, repeated)
 	}
 }
 
+func ExampleRepeat() {
+	repeated := Repeat("G", 10)
+	fmt.Println(repeated)
+	// Output: GGGGGGGGGG
+}
+
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 10)
 	}
 }
