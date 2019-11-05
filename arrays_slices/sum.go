ed0d65017297e944e1da2abb5b17b1999ed0babd
LH <lh@lhdv.net>
2019-11-05 17:00:31 -0300

    Array/Slice test
    - Add tests to SumAll() which will sum all elements from each slice
    passed.
    - Add tests to SummAllTails() which will sum from second elements until
    the slice end, from each slice passed.

--- arrays_slices/sum.go
+++ arrays_slices/sum.go
@@ -10,3 +10,31 @@ func Sum(numbers []int) int {
 
 	return sum
 }
+
+// SumAll will sum all elements for each slice and return them on a new slice
+func SumAll(numbers ...[]int) []int {
+
+	sumAll := make([]int, len(numbers))
+
+	for i, n := range numbers {
+		sumAll[i] = Sum(n)
+	}
+
+	return sumAll
+}
+
+// SumAllTails sums from the second item on slice until its end
+func SumAllTails(numbers ...[]int) []int {
+
+	sumAll := make([]int, len(numbers))
+
+	for i, n := range numbers {
+		if len(n) == 0 {
+			sumAll[i] = 0
+		} else {
+			sumAll[i] = Sum(n[1:])
+		}
+	}
+
+	return sumAll
+}
--- arrays_slices/sum_test.go
+++ arrays_slices/sum_test.go
@@ -1,6 +1,9 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 
@@ -13,6 +16,47 @@ func TestSum(t *testing.T) {
 		if got != want {
 			t.Errorf("got %d want %d, given %v", got, want, numbers)
 		}
+	})
+
+}
+
+func TestSumAll(t *testing.T) {
+
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		SumAll([]int{1, 2, 3, 4, 5}, []int{1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000})
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 1}, []int{0, 9, 1})
+		want := []int{3, 10}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{1, 2, 3})
+		want := []int{0, 5}
+
+		checkSums(t, got, want)
 	})
 
 }
