95d6cef358fa111cdae368f08aebc5cb7a7da370
LH <lh@lhdv.net>
2019-11-06 12:18:19 -0300

    Structs/Methods/Interfaces
    - Introduce Shape interface to help decoupling Area() method from
    Rectangle and Circle structs

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -14,8 +14,10 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkTest := func(t *testing.T, got, want float64) {
+	checkArea := func(t *testing.T, shape Shape, want float64) {
 		t.Helper()
+
+		got := shape.Area()
 
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
@@ -24,17 +26,15 @@ func TestArea(t *testing.T) {
 
 	t.Run("rectangles", func(t *testing.T) {
 		rect := Rectangle{12.0, 6.0}
-		got := rect.Area()
 		want := 72.0
 
-		checkTest(t, got, want)
+		checkArea(t, rect, want)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circ := Circle{10}
-		got := circ.Area()
 		want := 314.1592653589793
 
-		checkTest(t, got, want)
+		checkArea(t, circ, want)
 	})
 }
