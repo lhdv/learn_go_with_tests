f0d44dcf19d14788928b2a654cc6b372c765c6e5
LH <lh@lhdv.net>
2019-11-06 12:10:30 -0300

    Structs/Methods/Interfaces
    - Refactor to create Rectangle and Circle structures and Area() method
    for each one.

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,11 +13,28 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rect := Rectangle{12.0, 6.0}
-	got := Area(rect)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %v want %v", got, want)
+	checkTest := func(t *testing.T, got, want float64) {
+		t.Helper()
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rect := Rectangle{12.0, 6.0}
+		got := rect.Area()
+		want := 72.0
+
+		checkTest(t, got, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circ := Circle{10}
+		got := circ.Area()
+		want := 314.1592653589793
+
+		checkTest(t, got, want)
+	})
 }
