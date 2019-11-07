dce4cb94b04d9d21ae8efb2c4499da945c1ab33c
LH <lh@lhdv.net>
2019-11-07 16:33:46 -0300

    Pointers/Errors
    - Refactor Withdraw() error into a new variable and change its tests
    - Add Bitcoin type tests

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,25 +1,9 @@
 package pointers
 
+import "fmt"
 import "testing"
 
 func TestWallet(t *testing.T) {
-
-	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
-		t.Helper()
-
-		got := wallet.Balance()
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
-	}
-
-	assertError := func(t *testing.T, err error) {
-		if err == nil {
-			t.Error("wanted an error but didn't get one.")
-		}
-	}
-
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
@@ -44,6 +28,40 @@ func TestWallet(t *testing.T) {
 		err := wallet.Withdraw(Bitcoin(100))
 
 		assertBalance(t, wallet, startingBalance)
-		assertError(t, err)
+		assertError(t, err, ErrInsufficientFunds)
 	})
+}
+
+func TestBitcoin(t *testing.T) {
+	t.Run("Test Stringer() interface", func(t *testing.T) {
+		btc := Bitcoin(10)
+		got := fmt.Sprintf("%s", btc)
+		want := "10 BTC"
+
+		if got != want {
+			t.Errorf("got: %s, want: %s", got, want)
+		}
+	})
+}
+
+func assertBalance(t *testing.T, wallet Wallet, want Bitcoin) {
+	t.Helper()
+
+	got := wallet.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func assertError(t *testing.T, got, want error) {
+	t.Helper()
+	if got == nil {
+		// Fatal will stop the test when it's called
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got: %q, want: %q", got, want)
+	}
 }
