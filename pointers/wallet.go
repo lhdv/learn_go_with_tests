7d97c2a23d7d20a348b2b654ae876ec370e70c2b
LH <lh@lhdv.net>
2019-11-07 15:16:50 -0300

    Pointers/Errors
    - Add Bitcoin type(as an int) and replace the type of balance in Wallet
    struct
    - Implement Stringer interface on Bitcoin type

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,23 @@
 package pointers
 
-type Wallet struct {
-	balance int
+import (
+	"fmt"
+)
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Balance() int {
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
 }
 
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
