795ee382aec77e9c6f56dc591677b8a5f08b1acc
LH <lh@lhdv.net>
2019-11-07 15:43:14 -0300

    Pointers/Errors
    - Implement Withdraw() func adding tests
    - Refactor tests to remove repeated code

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,14 +3,32 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
 
-	got := wallet.Balance()
-	want := Bitcoin(10)
+		got := wallet.Balance()
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(10)
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(10)
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
 }
