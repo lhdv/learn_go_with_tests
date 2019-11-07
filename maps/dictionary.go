0bf1f2ee63afa35601f9d74b0d087e8659faa639
LH <lh@lhdv.net>
2019-11-07 17:38:32 -0300

    Maps
    - Create error variable when no word was found in dictionary
    - Refactor the tests and create helper methods

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,20 @@
 package maps
 
+import (
+	"errors"
+)
+
+var ErrNotFound = errors.New("could not find the word you were looking for")
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+func (d Dictionary) Search(word string) (string, error) {
+
+	definition, found := d[word]
+
+	if !found {
+		return "", ErrNotFound
+	}
+
+	return definition, nil
 }
