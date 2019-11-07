5c26907c6ae22d274969716735d03401358ece91
LH <lh@lhdv.net>
2019-11-07 16:46:24 -0300

    Pointers/Errors
    - Add missing test on Withdraw() when there's enough funds
    - Add assertNoError() to test something that should not require an error

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -16,10 +16,11 @@ func TestWallet(t *testing.T) {
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
 
-		wallet.Withdraw(10)
+		err := wallet.Withdraw(10)
 		want := Bitcoin(10)
 
 		assertBalance(t, wallet, want)
+		assertNoError(t, err)
 	})
 
 	t.Run("Withdraw insufficient founds", func(t *testing.T) {
@@ -63,5 +64,12 @@ func assertError(t *testing.T, got, want error) {
 
 	if got != want {
 		t.Errorf("got: %q, want: %q", got, want)
+	}
+}
+
+func assertNoError(t *testing.T, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error, but didn't want one")
 	}
 }
