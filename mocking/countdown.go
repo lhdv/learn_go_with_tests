367077ebfcecb427941f1bd45b5fb5572faa78ed
LH <lh@lhdv.net>
2019-11-08 18:22:26 -0300

    Mocking
    - Create a Sleeper interface to mock the Sleep() method in the tests to
    not require wait for 4 seconds to get the tests finish.

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -3,8 +3,28 @@ package mocking
 import (
 	"fmt"
 	"io"
+	"time"
 )
 
-func Countdown(out io.Writer) {
-	fmt.Fprintf(out, "3")
+const finalWorld = "Go!"
+const countDownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countDownStart; i > 0; i-- {
+		sleeper.Sleep()
+		fmt.Fprintln(out, i)
+	}
+
+	sleeper.Sleep()
+	fmt.Fprintln(out, finalWorld)
 }
