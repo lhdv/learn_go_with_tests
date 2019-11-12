6ecde3185424aecc3450608bff6493fff3a4ea99
LH <lh@lhdv.net>
2019-11-12 14:19:05 -0300

    Select
    - Create initial Racer() func to test the fastest url

---
+++ selecting/racer_test.go
@@ -0,0 +1,15 @@
+package selecting
+
+import "testing"
+
+func TestRacer(t *testing.T) {
+	slowURL := "http://www.theverge.com"
+	fastURL := "http://www.google.com"
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got: %q, want: %q", got, want)
+	}
+}
