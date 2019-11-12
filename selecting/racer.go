1e294e144c468b6e1a4221271b6d22ea8f30ee54
LH <lh@lhdv.net>
2019-11-12 15:03:08 -0300

    Select
    - Refactor code and tests to add select statment calling to make it
    simpler and more readable.

--- selecting/racer.go
+++ selecting/racer.go
@@ -1,23 +1,30 @@
 package selecting
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) string {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
+func Racer(a, b string) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
 
-	if aDuration < bDuration {
-		return a
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+
+	return ch
 }
