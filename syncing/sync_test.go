051256f2fc0860eef95de568222585ca93b94784
LH <lh@lhdv.net>
2019-11-22 15:15:25 -0300

    Sync
    - Create a assert func to help test Counter

--- syncing/sync_test.go
+++ syncing/sync_test.go
@@ -14,3 +14,11 @@ func TestCounter(t *testing.T) {
 		}
 	})
 }
+
+func assertCounter(t *testing.T, got Counter, want int) {
+	t.Helper()
+
+	if got.Value() != want {
+		t.Errorf("got: %d, want: %d", got.Value(), want)
+	}
+}
