0a0fcdfa6740a48a054cecb714ad09db6c17d1a8
LH <lh@lhdv.net>
2019-11-22 17:35:09 -0300

    Sync
    - Add code to handle concurrent execution and mutexes

--- syncing/sync_test.go
+++ syncing/sync_test.go
@@ -1,6 +1,9 @@
 package syncing
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestCounter(t *testing.T) {
 	t.Run("incremeting the counter 3 times leaves it at 3", func(t *testing.T) {
@@ -13,9 +16,32 @@ func TestCounter(t *testing.T) {
 			t.Errorf("got: %d, want: %d", counter.Value(), 3)
 		}
 	})
+
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := NewCounter()
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func(w *sync.WaitGroup) {
+				counter.Inc()
+				w.Done()
+			}(&wg)
+		}
+
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
 }
 
-func assertCounter(t *testing.T, got Counter, want int) {
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+func assertCounter(t *testing.T, got *Counter, want int) {
 	t.Helper()
 
 	if got.Value() != want {
