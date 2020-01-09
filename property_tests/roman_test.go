3e2012dd69404b188c31bcb14d2b5eb4ab6cf804
LH <lh@lhdv.net>
2020-01-09 12:15:49 -0300

    Property Tests
    - Refactor tests to not repeat code

--- property_tests/roman_test.go
+++ property_tests/roman_test.go
@@ -6,43 +6,26 @@ import (
 
 func TestConvertToRoman(t *testing.T) {
 
-	t.Run("Converting number 1", func(t *testing.T) {
-		want := "I"
-		got := ConvertToRoman(1)
-		if got != want {
-			t.Errorf("want %q got %q\n", want, got)
-		}
-	})
+	cases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to I", 2, "II"},
+		{"3 gets converted to I", 3, "III"},
+		{"4 gets converted to I", 4, "IV"},
+		{"5 gets converted to I", 5, "V"},
+	}
 
-	t.Run("Converting number 2", func(t *testing.T) {
-		want := "II"
-		got := ConvertToRoman(2)
-		if got != want {
-			t.Errorf("want %q got %q\n", want, got)
-		}
-	})
+	for _, test := range cases {
+		t.Run(test.Description, func(t *testing.T) {
+			want := test.Want
+			got := ConvertToRoman(test.Arabic)
+			if want != got {
+				t.Errorf("want: %q, got: %q", want, got)
+			}
+		})
+	}
 
-	t.Run("Converting number 3", func(t *testing.T) {
-		want := "III"
-		got := ConvertToRoman(3)
-		if got != want {
-			t.Errorf("want %q got %q\n", want, got)
-		}
-	})
-
-	t.Run("Converting number 4", func(t *testing.T) {
-		want := "IV"
-		got := ConvertToRoman(4)
-		if got != want {
-			t.Errorf("want %q got %q\n", want, got)
-		}
-	})
-
-	t.Run("Converting number 5", func(t *testing.T) {
-		want := "V"
-		got := ConvertToRoman(5)
-		if got != want {
-			t.Errorf("want %q got %q\n", want, got)
-		}
-	})
 }
