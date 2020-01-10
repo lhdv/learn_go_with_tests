60c9a1713941ed266b221141eb7b57c7b304a7c1
LH <lh@lhdv.net>
2020-01-10 16:25:38 -0300

    Property Tests
    - Last implementation refactor
    - Use test/quick and properties to check random values for input

--- property_tests/roman_test.go
+++ property_tests/roman_test.go
@@ -3,11 +3,12 @@ package roman
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
 var (
 	cases = []struct {
-		Arabic int
+		Arabic uint16
 		Roman  string
 	}{
 		{1, "I"},
@@ -60,5 +61,23 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("want: %d, got: %d", want, got)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		if arabic > 3999 {
+			return true
+		}
+		t.Log("testing", arabic)
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, &quick.Config{
+		MaxCount: 1000,
+	}); err != nil {
+		t.Error("failed checks", err)
 	}
 }
