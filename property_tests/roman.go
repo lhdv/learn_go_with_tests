0da16f37ec7e5b9dde855dbdc430addf30df74c0
LH <lh@lhdv.net>
2020-01-10 15:57:39 -0300

    Property Tests
    - First fully functional implementation

--- property_tests/roman.go
+++ property_tests/roman.go
@@ -69,7 +69,7 @@ func ConvertToArabic(roman string) int {
 				total += value
 				i++ // move past this character too for the next loop
 			} else {
-				total++
+				total += numerals.ValueOf(symbol)
 			}
 		} else {
 			total += numerals.ValueOf(symbol)
@@ -80,5 +80,6 @@ func ConvertToArabic(roman string) int {
 }
 
 func couldBeSubtractive(index int, currentSymbol uint8, roman string) bool {
-	return index+1 < len(roman) && currentSymbol == 'I'
+	isSubtractiveSymbol := currentSymbol == 'I' || currentSymbol == 'X' || currentSymbol == 'C'
+	return index+1 < len(roman) && isSubtractiveSymbol
 }
