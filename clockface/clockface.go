47e220f8d4ea1adf68e43ce61978fcba0f110bc3
LH <lh@lhdv.net>
2020-01-14 17:23:23 -0300

    Math
    - Add more tests to second hand and add auxiliary functions to handle
    time and radians

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -1,6 +1,9 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 // A Point represents a two dimensional Cartesian coordinate
 type Point struct {
@@ -12,4 +15,16 @@ type Point struct {
 // represented as a Point.
 func SecondHand(t time.Time) Point {
 	return Point{150, 60}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return (math.Pi / 30) * float64(t.Second())
+}
+
+func simpleTime(hours, minutes, seconds int) time.Time {
+	return time.Date(312, time.October, 28, hours, minutes, seconds, 0, time.UTC)
+}
+
+func testName(t time.Time) string {
+	return t.Format("15:04:05")
 }
