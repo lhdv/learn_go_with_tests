518cb700bcf4bcd41f03750f824f58a40be20ded
LH <lh@lhdv.net>
2020-01-24 14:48:23 -0300

    Application
    - Refactor main function to create a temp PlayerStore to be used on
    PlayerServer handler

--- http-server/main.go
+++ http-server/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 	"net/http"
-
-	httpserver "github.com/lhdv/learn_go_with_tests/http-server"
 )
 
+type inMemoryPlayerStore struct{}
+
+func (i *inMemoryPlayerStore) GetPlayerScore(name string) int {
+	return 123
+}
+
 func main() {
-	server := &httpserver.PlayerServer{}
+	server := &PlayerServer{&inMemoryPlayerStore{}}
 	if err := http.ListenAndServe(":5000", server); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
