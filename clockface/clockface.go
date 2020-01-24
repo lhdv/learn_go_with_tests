0799da482fcdfd703782ad3de7f5931d991eeeae
LH <lh@lhdv.net>
2020-01-23 22:44:05 -0300

    Math
    - Refactor hour hand calculation in radians

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -78,7 +78,7 @@ func SecondHand(t time.Time) Point {
 }
 
 func hoursInRadians(t time.Time) float64 {
-	return (math.Pi / (6 / float64(t.Hour()%12)))
+	return (minutesInRadians(t) / 12) + (math.Pi / (6 / float64(t.Hour()%12)))
 }
 
 func minutesInRadians(t time.Time) float64 {
