9c6f000805dffd6f6e7fd8804a4bafee6f764315
LH <lh@lhdv.net>
2020-01-24 13:32:34 -0300

    Application
    - Add more tests scenarios and implement them

--- http-server/server.go
+++ http-server/server.go
@@ -3,9 +3,23 @@ package httpserver
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 // PlayerServer server to handle GET/POST requests
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+
+	if player == "Pepper" {
+		fmt.Fprint(w, "20")
+		return
+	}
+
+	if player == "Floyd" {
+		fmt.Fprint(w, "10")
+		return
+	}
+
+	return
+
 }
