c3827b4d5b6ecafc83828c0e125dbc8a8787258b
LH <lh@lhdv.net>
2020-01-27 13:51:47 -0300

    Application
    - Refactor tests and implementation of RecordWin function to check how
    many players were stored abd if its names are equal.

--- http-server/server_test.go
+++ http-server/server_test.go
@@ -68,8 +68,10 @@ func TestStoreWins(t *testing.T) {
 	}
 	server := &PlayerServer{&store}
 
-	t.Run("it returns accepted on POST", func(t *testing.T) {
-		request := newPostWinRequest("Pepper")
+	t.Run("it records wins on POST", func(t *testing.T) {
+		player := "Pepper"
+
+		request := newPostWinRequest(player)
 		response := httptest.NewRecorder()
 
 		server.ServeHTTP(response, request)
@@ -77,9 +79,27 @@ func TestStoreWins(t *testing.T) {
 		assertStatus(t, response.Code, http.StatusAccepted)
 
 		if len(store.winCalls) != 1 {
-			t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+			t.Fatalf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
 		}
+
+		if store.winCalls[0] != player {
+			t.Errorf("did not store correct winner got %q want %q", store.winCalls[0], player)
+		}
+
 	})
+
+	// t.Run("it returns accepted on POST", func(t *testing.T) {
+	// 	request := newPostWinRequest("Pepper")
+	// 	response := httptest.NewRecorder()
+
+	// 	server.ServeHTTP(response, request)
+
+	// 	assertStatus(t, response.Code, http.StatusAccepted)
+
+	// 	if len(store.winCalls) != 1 {
+	// 		t.Errorf("got %d calls to RecordWin want %d", len(store.winCalls), 1)
+	// 	}
+	// })
 }
 
 func newPostWinRequest(name string) *http.Request {
