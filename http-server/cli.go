d55af59a3b09366779640855c05f5cd503365789
LH <lh@lhdv.net>
2020-01-31 14:21:12 -0300

    Application/IO
    - Refactor CLI tests to use an implementation to io.Reader to test
    input data

--- http-server/cli.go
+++ http-server/cli.go
@@ -1,11 +1,14 @@
 package poker
 
+import "io"
+
 // CLI struct to handle the command line application
 type CLI struct {
 	playerStore PlayerStore
+	in          io.Reader
 }
 
 // PlayPoker start a poker game
 func (c *CLI) PlayPoker() {
-	c.playerStore.RecordWin("Cleo")
+	c.playerStore.RecordWin("Chris")
 }
