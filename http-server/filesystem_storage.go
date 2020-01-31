777c946b395ee93e1da971a5f3d7ae179ad40447
LH <lh@lhdv.net>
2020-01-31 10:23:56 -0300

    Application/IO
    - Refactor NewFileSystemPlayerStore to extract initialising logic
    into a new private function

--- http-server/filesystem_storage.go
+++ http-server/filesystem_storage.go
@@ -16,20 +16,13 @@ type FileSystemPlayerStore struct {
 
 // NewFileSystemPlayerStore create a new store
 func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
-	file.Seek(0, 0)
 
-	info, err := file.Stat()
+	err := initPlayerDBFile(file)
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
-	}
-
-	if info.Size() == 0 {
-		file.Write([]byte(`[]`))
-		file.Seek(0, 0)
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
 	}
 
 	league, err := NewLeague(file)
-
 	if err != nil {
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
 	}
@@ -74,4 +67,20 @@ func (fs *FileSystemPlayerStore) RecordWin(name string) {
 	}
 
 	fs.database.Encode(fs.league)
+}
+
+func initPlayerDBFile(file *os.File) error {
+	file.Seek(0, 0)
+
+	info, err := file.Stat()
+	if err != nil {
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+	}
+
+	if info.Size() == 0 {
+		file.Write([]byte(`[]`))
+		file.Seek(0, 0)
+	}
+
+	return nil
 }
