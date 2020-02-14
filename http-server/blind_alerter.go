c811f44d179ac1fe4ae44450c67bd7ac0494330f
LH <lh@lhdv.net>
2020-02-14 16:40:17 -0300

    Application/Time
    - Add a type function for BlindAlerter interface to allow implement it
    using just a func

--- http-server/blind_alerter.go
+++ http-server/blind_alerter.go
@@ -1,8 +1,24 @@
 package poker
 
-import "time"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 // BlindAlerter is a implementation interface to wrap time.AfterFunc
 type BlindAlerter interface {
 	ScheduleAlertAt(duration time.Duration, amount int)
+}
+
+type BlindAlerterFunc func(duration time.Duration, amount int)
+
+func (a BlindAlerterFunc) ScheduleAlertAt(duration time.Duration, amount int) {
+	a(duration, amount)
+}
+
+func StdOutAlerter(duration time.Duration, amount int) {
+	time.AfterFunc(duration, func() {
+		fmt.Fprintf(os.Stdout, "Blind is now %d\n", amount)
+	})
 }
